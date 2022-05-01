c34edbb468c894fb464be41f83971b0a900514e6
alexisjust42 <justalexis@cy-tech.fr>
2022-05-01 19:37:26 +0200

    fixed construtor + smth new in main
    the string constructor was wrong, i also added a beging of a test in main

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -18,28 +18,28 @@ public class Coordinate {
 		byte y=0;
 		
 		switch(row){
-		case '0':
+		case '1':
 			x=0;
 			break;
-		case '1':
+		case '2':
 			x=1;
 			break;
-		case '2':
+		case '3':
 			x=2;
 			break;
-		case '3':
+		case '4':
 			x=3;
 			break;
-		case '4':
+		case '5':
 			x=4;
 			break;
-		case '5':
+		case '6':
 			x=5;
 			break;
-		case '6':
+		case '7':
 			x=6;
 			break;
-		case '7':
+		case '8':
 			x=7;
 			break;
 		}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);		
+		
 		Position jeu = new Position();
+		Game partie = new Game("tmptest");
+		
 		System.out.println(jeu);
+		
+		
+		System.out.println("Enter a coordinate");
+		
+		String str = sc.nextLine();		
+		Coordinate c1 = new Coordinate(str);
+		Coordinate c2 = null;
+		
+		// tmp
+		System.out.println(""+c1.getCol()+c1.getCol());
+		
+		while(true){
+			
+			System.out.println("here are the reachable cases");
+			byte[][] accessible = jeu.caseAccess(c1); // warning : this line also display the reachable cases
+			
+			
+
+			
+			str = sc.nextLine();
+			c2= new Coordinate(str);
+
+			if(accessible[c2.getRow()][c2.getCol()] > 0){	// test if c2 is a reachable destination
+				System.out.println("bravo, c'est un coup valide :");
+				jeu.playMove(c1,c2,partie);
+				System.out.println(jeu);
+				
+				System.out.println("joueur suivant, votre coup :");
+				str = sc.nextLine();
+				c1= new Coordinate(str);
+			}
+		  else{ 
+			  System.out.println("ce n'est pas une case accessible ...");
+			  c1 = c2;
+		  }
+		}
+		
+		
+		
+		
+		
+		
+		/*
 		System.out.println(jeu.getPosCase(new Coordinate((byte)6,(byte)2)));
 		//jeu.setTurn(false);
 		jeu.setEnPassant(new Coordinate((byte)2,(byte)2));
@@ -12,6 +62,6 @@ public class Main {
 		System.out.println(jeu);
 		jeu.caseAccess(new Coordinate((byte)3,(byte)3));
 		//Game game = new Game("ss");
-		//boolean b = game.timedOut();
+		//boolean b = game.timedOut();		-->*/
 	}
 }
